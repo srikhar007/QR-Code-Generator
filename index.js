e55d188db7390e5fec7cba479dd79a380f0c384a
srikhar <103771552+srikhar007@users.noreply.github.com>
2024-09-16 16:46:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let imageBox=document.getElementById('image');
+let qrImage=document.getElementById('qrImage');
+let qrText=document.getElementById('qrText');
+
+
+
+function generateQR(){
+    qrImage.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+}
